8fab18b342c30e7667899947457f321b0509ae19
YCChen <victor.chen@openapply.com>
2022-09-10 18:14:45 +0800

    successfully add different component to differet routes

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Canvas } from '@react-three/fiber'
+import Cat from './components/cat.jsx'
+import Planet from './components/planet.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense } from 'react'
+import Background from './components/background.jsx'
+import CameraController from './components/camera_controller.jsx'
+
+function App() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Canvas
+        flat linear
+        camera={{ fov: 75, position: [0, 0, 20]}}
+      >
+        <CameraController />
+        {/* <Suspense>
+          <Background />
+        </Suspense> */}
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Planet position={[0, 0, 0]} />} />
+            <Route path="/foo" element={<Cat position={[4, 0, 0]} />} />
+          </Routes>
+        </Router>
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Cat from './components/cat.jsx'
-import Planet from './components/planet.jsx'
-import Background from './components/background.jsx'
-import CameraController from './components/camera_controller.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <div style={{ width: "100%", height: "100vh" }}>
-    <BrowserRouter>
-      <Canvas
-        flat linear
-        camera={{ fov: 75, position: [50, 5, 30]}}
-      >
-        <Suspense>
-          <Background />
-        </Suspense>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        {/* <Planet position={[0, 0, 0]} /> */}
-        {/* <Cat position={[-0, -20, -20]} /> */}
-      </Canvas>
-      <Routes>
-        {/* <Route path="/" component={<Planet position={[-1.2, 0, 0]} />} /> */}
-      </Routes>
-    </BrowserRouter>
-  </div>
+  <Router>
+    <App />
+  </Router>
 )
